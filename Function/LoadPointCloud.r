1cab9ef83e7c7df9cc4c0eb1d2c31c90e6dc9289
Simon Filhol <svfilhol@alaska.edu>
2012-07-31 20:06:43 -0800

    adapting function to windows and change while loop into repeat

--- Function/LoadPointCloud.r
+++ Function/LoadPointCloud.r
@@ -4,17 +4,25 @@
 LoadPointCloud <- function(){
 
 library("tcltk")
-my.dir.orig <- tk_choose.dir(default = "/Users/simonfilhol", caption = "Select the pictures directory")
+
+# Test the type of OS, windows or mac
+if(Sys.info()['sysname']=="Windows"){
+  my.dir.orig <- tk_choose.dir(default = "C:/Documents and Settings", caption = "Select the file directory")
+}
+if(Sys.info()['sysname']=="Darwin"){
+my.dir.orig <- tk_choose.dir(default = "/Users", caption = "Select the file directory")
+}
 my.file.name <- dir(my.dir.orig)
 
-OK <- 0
-while (OK==0){
+
+repeat {
   print("Which file to use? ")
 print(my.file.name)
 ind <- as.numeric(readline("Give row number for file to open: "))
 my.file <- my.file.name[ind]
 print(my.file)
 OK <- as.numeric(readline("Is it the right file (0/1)? "))
+  if(OK>0){break}
 }
 
 
@@ -22,16 +30,17 @@ OK <- as.numeric(readline("Is it the right file (0/1)? "))
 raw.data <- read.table( paste(my.dir.orig,"/",my.file,sep=""), quote="\"")
 my.data <- matrix(c(raw.data[,1],raw.data[,2],raw.data[,3]),ncol=3)
 rm(raw.data)
+my.data <- round(my.data,3)
 
 # Translate data to reduce memory use
 if(my.data[1,1]>500){
 my.trans.xyz <- my.data[1,]
-print(my.trans.xyz)
+print(paste("Translation apply =",my.trans.xyz))
 my.data <- cbind(my.data[,1]-my.trans.xyz[1], my.data[,2]-my.trans.xyz[2],my.data[,3]-my.trans.xyz[3])
-my.info <- list(Translation=my.trans.xyz,Data= my.data)
+my.info <- list(Directory=my.dir.orig,Filelist=my.file.name,Translation=my.trans.xyz,Data= my.data)
 }
 else{
-  my.info <- list(Data= my.data) 
+  my.info <- list(Directory=my.dir.orig,Filelist=my.file.name,Data= my.data) 
 }
 return(my.info)
 }
